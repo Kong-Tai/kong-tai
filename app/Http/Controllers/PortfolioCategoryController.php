2e1f4421e464958b844a8e1a4dfbe37064d24e0b
kiprutobrian <briankipruto793@gmail.com>
2019-11-06 19:45:31 +0300

    portfolio category

---
+++ app/Http/Controllers/PortfolioCategoryController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortfolioCategoryController extends Controller
+{
+    //
+}
---
+++ database/migrations/2019_11_06_164453_create_portfolio_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfolioCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolio_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolio_categories');
+    }
+}
