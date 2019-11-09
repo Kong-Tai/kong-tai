60cb680c62c2c09e94b4e6330f1337b5f5989b5d
kiprutobrian <briankipruto793@gmail.com>
2019-11-09 14:20:26 +0300

    nova resources

---
+++ app/Nova/Portfolio.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Portfolio extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Portfolio';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'title', 'client', 'user_id', 'category_id'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Title')->sortable(),
+            Text::make('Slug')->sortable(),
+            Image::make('hero_image'),
+            Text::make('intro')->sortable(),
+            Text::make('client')->sortable(),
+            Text::make('description')->sortable(),
+            Text::make('website_url')->sortable(),
+            Text::make('workdate')->sortable(),
+            Text::make('linkedin_url')->sortable(),
+            Text::make('twitter_url')->sortable(),
+            Text::make('facebook_url')->sortable(),
+            Text::make('category_id')->sortable(),
+            ID::make('user_id')->sortable(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
