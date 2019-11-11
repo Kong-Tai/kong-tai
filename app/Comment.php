0699ec4f3670f0581d6370068543e7cfba681dca
kiprutobrian <briankipruto793@gmail.com>
2019-11-11 14:44:23 +0300

    comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,13 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
     public function replies()
     {
-        return $this->hasMany(Comment::class, 'parent_id');
+        return $this->hasMany(Reply::class);
     }
 }
