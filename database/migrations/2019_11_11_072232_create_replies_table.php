91002ac4177e29c1f6948833475cbec6313c881c
kiprutobrian <briankipruto793@gmail.com>
2019-11-11 14:45:59 +0300

    replies migration

---
+++ database/migrations/2019_11_11_072232_create_replies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable()->unsigned();
+            $table->integer('comment_id')->nullable()->unsigned();
+            $table->text('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
